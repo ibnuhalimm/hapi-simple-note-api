dda651e3d387e6dbeaf7b0b011c5859c76ec4e48
Ibnu Halim Mustofa <ibnuhalimm@gmail.com>
2021-07-22 16:01:37 +0700

    install and configure hapi-js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,13 @@
-console.log('It works!');
+import Hapi from '@hapi/hapi';
+
+const init = async () => {
+    const server = Hapi.server({
+        port: 5000,
+        host: 'localhost',
+    });
+
+    await server.start();
+    console.log(`Server run on ${server.info.uri}`);
+};
+
+init();
