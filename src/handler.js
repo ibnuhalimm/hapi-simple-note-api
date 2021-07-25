d644787a2fef21985272d11b04b103c0fa616b7d
Ibnu Halim Mustofa <ibnuhalimm@gmail.com>
2021-07-25 08:36:47 +0700

    delete note API

--- src/handler.js
+++ src/handler.js
@@ -108,4 +108,37 @@ const editNoteByIdHandler = (request, h) => {
 };
 
 
-module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler };
+const deleteNoteByIdHandler = (request, h) => {
+    const { id } = request.params;
+
+    const index = notes.findIndex((note) => note.id === id);
+
+    if (index !== 1) {
+        notes.splice(index, 1);
+
+        const response = h.response({
+            status: 'success',
+            message: 'Note berhasil dihapus',
+        });
+        response.code = 200;
+
+        return response;
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Note gagal dihapus. ID tidak ditemukan',
+    });
+    response.code = 404;
+
+    return response;
+};
+
+
+module.exports = {
+    addNoteHandler,
+    getAllNotesHandler,
+    getNoteByIdHandler,
+    editNoteByIdHandler,
+    deleteNoteByIdHandler,
+};
